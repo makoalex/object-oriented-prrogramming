0592a32d9fa9f6accd47e4f4c9cb6ab2751396cd
Alexandra Macovei <macovei.a2@gmail.com>
2020-05-20 13:59:47 +0200

    cleaning the code with functions

--- PirateGame/game_01.py
+++ PirateGame/game_01.py
@@ -1,22 +1,41 @@
-import os
+MENU_SEPARATOR = '---------------------------'
+GAME_START = 'Let the games begin!!'
 
-print('Taipan! Welcome')
-firm_name = input('What will you name your firm? \n')
+
+def welcome():
+    print('Taipan! Welcome')
+
+
+def firm_name():
+    firm_name = input('What will you name your firm? \n')
+    return firm_name
+
+
+def start_option():
+    starting_option = input(
+        'would you like to start...\n 1) With cash (and a debt)\n >>or<<\n 2) Five guns and no cash...\n')
+
+    if starting_option == '1':
+        cash = 400
+        debt = cash
+        guns = 0
+
+    else:
+        cash = 0
+        guns = 5
+        debt = cash
+    return cash, debt, guns
+
+
+# GAME START
+welcome()
+firm_name = firm_name()
+cash, debt, guns = start_option()
+print('\n' * 10)
+# MENU START
+print(MENU_SEPARATOR)
+print(GAME_START)
+print(MENU_SEPARATOR)
+
 print('Firm: {}'.format(firm_name))
-starting_option = input(
-    'would you like to start...\n 1) With cash (and a debt)\n >>or<<\n 2) Five guns and no cash...\n')
-
-if starting_option == '1':
-    cash = 400
-    debt = cash
-    guns = 0
-else:
-    cash = 0
-    guns = 5
-    debt = cash
-os.system('cls')
-print('\n'*90)
-print('---------------------------')
-print('Let the games begin!!')
-print('---------------------------')
 print('cash: {}\ndebt: {}\nguns: {}'.format(cash, debt, guns))
