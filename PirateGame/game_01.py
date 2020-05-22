3f71a9738ffcec6ebc34f7cd09aa33f250ffd76c
Alexandra Macovei <macovei.a2@gmail.com>
2020-05-22 10:42:34 +0200

    adding the while loop and functions

--- PirateGame/game_01.py
+++ PirateGame/game_01.py
@@ -1,5 +1,10 @@
+import datetime
+import os
+from game_02_content import leave_port, sell, buy, transfer_bank
 MENU_SEPARATOR = '---------------------------'
 GAME_START = 'Let the games begin!!'
+Date = datetime.date(1850, 4, 13)
+
 
 
 def welcome():
@@ -9,7 +14,6 @@ def welcome():
 def firm_name():
     firm_name = input('What will you name your firm? \n')
     return firm_name
-
 
 def start_option():
     starting_option = input(
@@ -27,15 +31,33 @@ def start_option():
     return cash, debt, guns
 
 
+
 # GAME START
 welcome()
 firm_name = firm_name()
 cash, debt, guns = start_option()
 print('\n' * 10)
-# MENU START
-print(MENU_SEPARATOR)
-print(GAME_START)
-print(MENU_SEPARATOR)
+while True:
+    os.system('cls')
+    # MAIN GAME INTERFACE
+    print(MENU_SEPARATOR)
+    print(GAME_START)
+    print(MENU_SEPARATOR)
 
-print('Firm: {}'.format(firm_name))
-print('cash: {}\ndebt: {}\nguns: {}'.format(cash, debt, guns))
+    print('Firm: {}'.format(firm_name))
+    print('Date: {}'.format(Date))
+    print('cash: {}\ndebt: {}\nguns: {}'.format(cash, debt, guns))
+    print(MENU_SEPARATOR)
+    print('WHAT WILL YOU CHOSE NOW?:\nL)eave Port\nS)ell\nB)uy\nV)isit the Money Lender\nT)ransfer Bank\nQ)uit')
+    new_option = input('enter the next step of your journey')
+
+    if new_option == 'L'.lower():
+        leave_port()
+    elif new_option == 'S'.lower():
+        sell()
+    elif new_option == 'B'.lower():
+        buy()
+    elif new_option == 'T'.lower():
+        transfer_bank()
+    elif new_option == 'Q'.lower():
+        break
---
+++ PirateGame/game_02_content.py
@@ -0,0 +1,14 @@
+def leave_port():
+    pass
+
+
+def sell():
+    input('what would you like to sell?\n')
+
+
+def buy():
+    input('what would you like to buy\n')
+
+
+def transfer_bank():
+    pass
